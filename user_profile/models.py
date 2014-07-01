cbe93ad309b742e7f711e4f4723c033341f9d14a
Can <cankoc63@gmail.com>
2014-07-01 00:12:35 -0700

    changes to models.py in user profile

--- user_profile/models.py
+++ user_profile/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
+    phone_number = models.IntegerField(max_length=1, default=1)
+    start_date = models.DateField(_(Date), default=datetime.date.today)    
 
     def __str__(self):
         return self.user.first_name + " " + self.user.last_name
+    
