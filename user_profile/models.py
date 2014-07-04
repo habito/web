f1726eee773f1c8a383c6da4865c328e4d435c90
Can <cankoc63@gmail.com>
2014-07-03 21:50:36 -0700

    added startdate, enddate and also income.

--- user_profile/models.py
+++ user_profile/models.py
@@ -6,7 +6,9 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User)
     phone_number = models.IntegerField()
     start_date = models.DateField(default=datetime.date.today)    
-    	
+    end_date = models.DateField()
+    annual_pay = models.IntegerField()
+    
     def __str__(self):
         return self.user.first_name + " " + self.user.last_name
     
