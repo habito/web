533fada93eee906f361411ab90947436e576123c
Brian Su <briansudev@gmail.com>
2014-07-03 23:30:56 -0700

    navbar with different colors

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,5 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'website.views.index', name='index'),
+    url(r'^browse/$', 'website.views.browse', name='browse'),
 )
--- website/views.py
+++ website/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'website/index.html', {})
+
+def browse(request):
+    return render(request, 'website/browse.html', {})
