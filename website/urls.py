08c916abb921a8e2eb8c0acceee79f84120d9d7a
Can <cankoc63@gmail.com>
2014-07-06 18:36:40 -0700

    added login url.

--- website/urls.py
+++ website/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     url(r'^$', 'website.views.index', name='index'),
     url(r'^browse/$', 'website.views.browse', name='browse'),  
     url(r'^sign_up/$', 'website.views.sign_up', name='sign_up'),
+    url(r'^login/$', 'website.views.login_user', name='login_user'),
 )
