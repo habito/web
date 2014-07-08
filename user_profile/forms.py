0597e35284dd1778b7791f40c998f1e9cbfce69d
Can <cankoc63@gmail.com>
2014-07-08 01:02:20 -0700

    no edits

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -4,15 +4,26 @@ from user_profile.models import *
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
+    	
     def __init__(self, *args, **kwargs):
         super(forms.ModelForm, self).__init__(*args, **kwargs)
         self.fields['username'].help_text = None
         self.fields['email'].label = 'Email'
+        self.fields['password'].required = False
+        self.fields['confirm_password'].required = False
+    
+    def clean(self):
+        password1 = self.cleaned_data['password']
+        password2 = self.cleaned_data['confirm_password']
+        if password1 != password2:
+            raise forms.ValidationError("Your passwords didn't match.")
+        return self.cleaned_data
 
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'password')
-
+	
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
