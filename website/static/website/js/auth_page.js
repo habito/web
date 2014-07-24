ffc590c6692539eac16b6462afa3c8078f657293
Brian Su <briansudev@gmail.com>
2014-07-23 21:46:12 -0700

    made sign up and login page good for mobile

--- website/static/website/js/auth_page.js
+++ website/static/website/js/auth_page.js
@@ -1,9 +1,17 @@
 $(window).resize(function(){
-  $('.auth-div').css({
-    position:'absolute',
-    top: ($(window).height() - $('.auth-div').outerHeight())/2,
-    left: ($(window).width() - $('.auth-div').outerWidth())/2
-  });
+    if ($(window).height() <= 600) {
+        $('.auth-div').css({
+            position:'relative',
+            top: 'auto', 
+            left: ($(window).width() - $('.auth-div').outerWidth())/2
+        });
+    } else {
+        $('.auth-div').css({
+            position:'absolute',
+            top: ($(window).height() - $('.auth-div').outerHeight())/2,
+            left: ($(window).width() - $('.auth-div').outerWidth())/2
+        });
+    }
 });
 
 //To initially run the function:
