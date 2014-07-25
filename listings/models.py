7a2ee87dfb13c24c2846e55380655af5f0d2514d
Brian Su <briansudev@gmail.com>
2014-07-25 12:04:48 -0700

    added timeline to home page

--- listings/models.py
+++ listings/models.py
@@ -2,16 +2,16 @@ from django.db import models
 from general_models.models import Address
 from user_profile.models import UserProfile
 
-class Apartment(models.Model):
+class Place(models.Model):
     address = models.OneToOneField(Address)
     num_beds = models.IntegerField(max_length=1, default=1)
     num_baths = models.IntegerField(max_length=1, default=1)
     num_stories = models.IntegerField(max_length=1, default=1)
       
-class AptListing(models.Model):
+class Listing(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
-    apt = models.IntegerField('Apartment')
+    apt = models.IntegerField('Place')
     owner = models.OneToOneField(UserProfile)
 
     features = models.OneToOneField('Features')
