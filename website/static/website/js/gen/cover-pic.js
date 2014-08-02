b61f690dbffd1734de1cf005436f7ec130cd1ba7
Brian Su <briansudev@gmail.com>
2014-08-01 17:17:24 -0700

    removed snow from pic list

--- website/static/website/js/gen/cover-pic.js
+++ website/static/website/js/gen/cover-pic.js
@@ -1,5 +1,5 @@
 function loadCoverPic(container, static_url) {
-	var photos = ['snow', 'balloon', 'craft', 'gg', 'narrow', 'office_bw', 'water'];
+	var photos = ['balloon', 'craft', 'gg', 'narrow', 'office_bw', 'water'];
 	var randomImage = photos[Math.floor(Math.random() * photos.length)];
 	var imageURL = 'website/img/banner/' + randomImage + '_low_opa_compressed.jpg';
 	$(container).css("background", "url(" + static_url + imageURL + ") no-repeat center center fixed");
